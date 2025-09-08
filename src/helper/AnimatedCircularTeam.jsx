ed65e2f3688dc4dc841c5b98a57a0514f7d945b4
Iraiyanbust <iraiyanbust@gmail.com>
2025-09-08 20:26:12 +0530

    updated photos

--- src/helper/AnimatedCircularTeam.jsx
+++ src/helper/AnimatedCircularTeam.jsx
@@ -1,4 +1,26 @@
+import photo1 from "../assets/team/IRAIYANBU.jpg";
+import photo2 from "../assets/team/LOKESHWARAN.jpg";
+import photo3 from "../assets/team/YUKESH_EDITOR.jpg";
+import photo4 from "../assets/team/Vignesh.jpg";
+import photo5 from "../assets/team/Ramu .jpg";
+import photo6 from "../assets/team/YUKESH_EDITOR.jpg";
+import photo7 from "../assets/team/dominic.jpg";
+
+
+
+
 import React, { useEffect, useRef } from "react";
+
+// 7 photos from public/images folder
+const cards = [
+  { id: 1, name: "Card 1", image: photo1 },
+  { id: 2, name: "Card 2", image: photo2 },
+  { id: 3, name: "Card 3", image: photo3 },
+  { id: 4, name: "Card 4", image: photo4 },
+  { id: 5, name: "Card 5", image: photo5 },
+  { id: 6, name: "Card 6", image: photo6 },
+  { id: 7, name: "Card 7", image: photo7 },
+];
 
 const HorizontalCylindricalCarousel = ({ cards }) => {
   const containerRef = useRef(null);
@@ -8,7 +30,7 @@ const HorizontalCylindricalCarousel = ({ cards }) => {
     let rotationY = 0;
 
     const rotate = () => {
-      rotationY = (rotationY + 0.4) % 360; // rotation speed
+      rotationY += 0.5; // slower speed for smooth rotation
       if (containerRef.current) {
         containerRef.current.style.transform = `perspective(1000px) rotateY(${rotationY}deg)`;
       }
@@ -17,23 +39,19 @@ const HorizontalCylindricalCarousel = ({ cards }) => {
 
     rotate();
 
-    return () => {
-      cancelAnimationFrame(frameId);
-    };
+    return () => cancelAnimationFrame(frameId);
   }, []);
 
   const cardCount = cards.length;
-  const theta = 360 / cardCount;
-  const radius = 220; // radius of cylinder
+  const theta = 360 / cardCount; // angle between cards
+  const radius = 220; // distance from center
 
   return (
-    <div className="w-96 h-56 mx-auto mt-40 perspective-1000 sm:perspective-1000 overflow-visible">
+    <div className="w-96 h-56 mx-auto mt-40 overflow-visible">
       <div
         ref={containerRef}
         className="relative w-full h-full origin-center transition-transform"
-        style={{
-          transformStyle: "preserve-3d",
-        }}
+        style={{ transformStyle: "preserve-3d" }}
       >
         {cards.map((card, i) => {
           const rotateY = i * theta;
@@ -61,4 +79,9 @@ const HorizontalCylindricalCarousel = ({ cards }) => {
   );
 };
 
-export default HorizontalCylindricalCarousel;
+// Use the carousel
+const App = () => {
+  return <HorizontalCylindricalCarousel cards={cards} />;
+};
+
+export default App;
